b9175c405f9b92d54b7d38f188b45c5f0e3338a4
ledewrt <61609258+ledewrt@users.noreply.github.com>
2020-06-08 10:56:19 +0800

    Update 18.06_X64_Lite_From_CTCGFW.sh

--- 18.06_X64_Lite_From_CTCGFW.sh
+++ 18.06_X64_Lite_From_CTCGFW.sh
@@ -13,20 +13,20 @@
 #添加固件版本描述
 rm -Rf package/lean/default-settings/files/zzz-default-settings
 rm -Rf package/lean/luci-app-ssr-plus
-rm -Rf package/lean/luci-app-ssr-plus  
+rm -Rf package/ntlf9t/luci-app-eqos
 #bcm27xx-userland为树莓派专用，需要移除
 rm -Rf package/utils/bcm27xx-userland
+#rm -Rf package/lienol/luci-app-control-webrestriction
+#rm -Rf package/ctcgfw/open-app-filter
 cp -Rf ../diy-lean/* ./
 sed -i 's/OpenWrt/LedeWrt Lite/g' package/lean/default-settings/files/zzz-default-settings
-rm -Rf package/my/luci-app-koolproxyR
+
 # 修改默认IP为192.168.168.1
 #sed -i 's/192.168.1.1/192.168.168.1/g' package/base-files/files/bin/config_generate
 #修改主机名
 #sed -i 's/'OpenWrt'/'LEDE'/g' package/base-files/files/bin/config_generate
 # 关闭禁止解析IPv6 DNS 记录
-sed -i '/option filter_aaaa 1/d' package/network/services/dnsmasq/files/dhcp.conf
-#使用smartdns是需要将dhcp里的dns缓存设置为0.
-sed -i '/option noresolv '1'/a option cachesize '0'' package/network/services/dnsmasq/files/dhcp.conf
+#sed -i '/option filter_aaaa 1/d' package/network/services/dnsmasq/files/dhcp.conf
 #修改默认主题
 #sed -i '/lang=zh_cn/i uci set luci.main.mediaurlbase=/luci-static/argon' package/lean/default-settings/files/zzz-default-settings
 #删除默认密码
@@ -43,8 +43,9 @@ sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d)-LEDE-1806-Lite-/g' incl
 git clone https://github.com/ledewrt/luci-app-eqos.git  package/luci-app-eqos
 git clone https://github.com/jefferymvp/luci-app-koolproxyR.git  package/luci-app-koolproxyR
 #git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+svn co https://github.com/ledewrt/openwrt-package/trunk/others/luci-app-control-webrestriction package/luci-app-control-webrestriction
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns  package/luci-app-smartdns
 ./scripts/feeds update -a
 ./scripts/feeds install -a
