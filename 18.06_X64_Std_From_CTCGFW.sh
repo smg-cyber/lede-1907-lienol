bcfb38f6c55d1a0cce790b71b4abe23076b8d7c5
ledewrt <61609258+ledewrt@users.noreply.github.com>
2020-06-14 10:45:45 +0800

    Update 18.06_X64_Std_From_CTCGFW.sh

--- 18.06_X64_Std_From_CTCGFW.sh
+++ 18.06_X64_Std_From_CTCGFW.sh
@@ -21,7 +21,7 @@ rm -Rf package/lienol/luci-app-webrestriction
 rm -Rf package/lienol/luci-app-weburl
 rm -Rf package/lienol/luci-app-timewol
 rm -Rf package/ctcgfw/luci-app-adguardhome
-rm -Rf package/lean/luci-app-flowoffload
+#rm -Rf package/lean/luci-app-flowoffload
 cp -Rf ../diy-lean/* ./
 sed -i 's/OpenWrt/LedeWrt Std/g' package/lean/default-settings/files/zzz-default-settings
 
@@ -62,7 +62,7 @@ svn co https://github.com/ledewrt/openwrt-package/trunk/others/luci-app-control-
 svn co https://github.com/ledewrt/openwrt-package/trunk/others/luci-app-control-timewol package/luci-app-control-timewol
 svn co https://github.com/ledewrt/openwrt-package/trunk/others/luci-app-control-weburl package/luci-app-control-weburl
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns  package/luci-app-smartdns
-#克隆Ameykyl的flowoffload，ctcgfw的folwoffload会导致adghome启动失败而且没有内存运行模式。
-svn co https://github.com/Ameykyl/openwrt18.06/trunk/package/lean/luci-app-flowoffload package/luci-app-flowoffload
+#克隆Ameykyl的flowoffload，ctcgfw的folwoffload会导致adghome启动失败而且没有内存运行模式（现已经不用，改为smartdns去广告）。
+#svn co https://github.com/Ameykyl/openwrt18.06/trunk/package/lean/luci-app-flowoffload package/luci-app-flowoffload
 ./scripts/feeds update -a
 ./scripts/feeds install -a
