dcad1896818a271dfddca4dd39642fe31de93b29
ledewrt <61609258+ledewrt@users.noreply.github.com>
2020-06-17 15:16:01 +0800

    Delete smartdns.sh

--- config-files/etc/smartdns/smartdns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-        flag=$(ps | grep /usr/sbin/smartdns | grep -v "grep" | wc -l)
-         if [ $flag = "0" ]
-        then
-          /usr/sbin/smartdns -c /var/etc/smartdns/smartdns.conf
-        fi
